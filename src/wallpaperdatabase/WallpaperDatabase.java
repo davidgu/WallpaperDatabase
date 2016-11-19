7c0f7c8bdac164acf18fdb66397ce9f24aabe26d
David <david.gu@hotmail.com>
2016-11-19 02:44:59 -0500

    Added image downloading

--- src/wallpaperdatabase/WallpaperDatabase.java
+++ src/wallpaperdatabase/WallpaperDatabase.java
@@ -14,18 +14,54 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 import java.util.regex.*;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import java.awt.image.*;
 
 public class WallpaperDatabase {
 
     /**
      * @param args the command line arguments
      */
+    
+    static int fileIndex = 0;
+    
     public static void main(String[] args) {
         // TODO code application logic here
         
+        Set<String> wallpaperUrls = new HashSet<>();
         
         
-        parseForImgur(downloadWebpageData("https://www.reddit.com/r/wallpapers/"));
+        System.out.println("Wallpaper Database V1.0.0");
+        System.out.println("0. Shutdown Program"+"\n"+
+                "1. Refresh URL Data"+"\n"+
+                "2. Download images"+"\n"+
+                "3. Analyze Images");
+        
+        Scanner sc = new Scanner(System.in);
+        
+        boolean isRunning = true;
+        
+        while(isRunning){
+            int choice = sc.nextInt();
+            switch(choice){
+                case 0:
+                    isRunning = false;
+                    break;
+                case 1:
+                    wallpaperUrls=parseForImgur(downloadWebpageData("https://www.reddit.com/r/wallpapers/"));
+                    System.out.println("Download Completed");
+                    break;
+                
+                case 2:
+                    Iterator urlIterator = wallpaperUrls.iterator();
+                    downloadImage(urlIterator.next().toString());
+                    System.out.println("Image Downloaded");
+                
+                default:
+                    break;
+            }
+        }
     }
     
     
@@ -44,14 +80,11 @@ public class WallpaperDatabase {
            
            while((dataBuffer = in.readLine())!=null){
                htmlData = htmlData.concat(dataBuffer);
-               //System.out.println(dataBuffer);
-               System.out.println("Downloaded Data");
            }
         }
         catch(Exception e){
             System.out.println(e.toString());
         }
-        System.out.println(htmlData);
         return htmlData;
     }
     
@@ -66,10 +99,35 @@ public class WallpaperDatabase {
         while(matchedUrls.find()){
             imgurUrls.add(matchedUrls.group());
         }
-        
-        System.out.println("Removed all characters but imgur urls");
-        
+
        return imgurUrls; 
+    }
+    
+    static void downloadImage(String stringUrl){
+        
+        BufferedImage image = null;
+        try{
+            URL url = new URL(stringUrl);
+            image = ImageIO.read(url);
+            File imageFile = new File(getFileName().concat(".").concat(getFileFormat(stringUrl)));
+            imageFile.createNewFile();
+            System.out.println(imageFile.getAbsolutePath());
+            ImageIO.write(image, getFileFormat(stringUrl), imageFile);
+        }
+        catch(Exception e){
+            System.out.println(e.toString());
+        }
+    }
+    
+    static String getFileFormat(String stringUrl){
+        String fileFormat = stringUrl.substring(stringUrl.length()-3, stringUrl.length()); //Return last 3 letters in URL, thus the file format
+        return fileFormat;
+    }
+    
+    static String getFileName(){
+        String fileName = Integer.toString(fileIndex);
+        WallpaperDatabase.fileIndex++;
+        return fileName;
     }
     
 }
